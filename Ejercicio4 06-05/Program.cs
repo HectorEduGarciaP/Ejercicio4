b554f6f0a329d810abe71434ec0760b6c4dc29cd
Hector Garcia <hgarciap12@miumg.edu.gt>
2023-05-06 13:00:39 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio4 06-05/Program.cs
@@ -0,0 +1,34 @@
+﻿using Ejercicio4_Interfaces.Mis_Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4_Interfaces.Mis_Clases
+{
+    internal class ClsPersona : IPersona
+    {
+
+        public ClsPersona(string nombre, int edad)
+        {
+        }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public void cumplirAños()
+        {
+            Console.WriteLine("Hoy es mi cumpleaños");
+            Edad++;
+            Console.WriteLine($"Mi edad actual es de: ");
+
+        }
+
+        public void DecirMiNombre()
+        {
+            Console.WriteLine($"Mi nombre es .");
+        }
+
+
+    }
+}
